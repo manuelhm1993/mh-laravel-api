da8ef0ec7acc0d9f5322c8f5b8b4416c17869bba
Manuel Henriquez <manuelhm1993@gmail.com>
2025-05-19 03:35:38 -0400

    Primer endpoint probado y funcionando, se creó el método para devolver todos los clientes

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -12,7 +12,9 @@ class ClientController extends Controller
      */
     public function index()
     {
-        //
+        $clients = Client::all();
+
+        return response()->json($clients);
     }
 
     /**
